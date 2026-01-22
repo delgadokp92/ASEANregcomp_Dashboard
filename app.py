69daf8c3a49f25f0290d9f65c3033bcbcc55aec8
delgadokp92 <73928924+delgadokp92@users.noreply.github.com>
2026-01-22 14:06:11 +0800

    Update app.py
    demonstration

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import streamlit as st
 # App config
 # =========================
 st.set_page_config(page_title="ASEAN Regulatory Dashboard", layout="wide")
-st.title("ASEAN Regulatory Dashboard (c/o PPDD)")
+st.title("ASEAN Regulatory Dashboard")
 
 DATA_FILE = "CBregs.xlsx"  
 
